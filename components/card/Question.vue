113f2c86abd26cdc9c312c364f3811b1c52a7127
dangdinhtai0001@gmail.com <dangdinhtai0001>
2021-10-28 00:10:57 +0700

    create question card

---
+++ components/card/Question.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-divider content-position="left">
+      <div class="font-bold text-lg">
+        {{ title }}
+      </div>
+    </el-divider>
+
+    <div class="text-base font-normal">
+      {{ content }}
+    </div>
+
+    <div class="my-3"></div>
+
+    <el-input placeholder="" v-model="value"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+    value: {
+      required: true,
+    },
+  },
+};
+</script>
--- pages/Evaluation/Self/index.vue
+++ pages/Evaluation/Self/index.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
-    <page-default :data="pageData">
-      <!-- ======== title ======== -->
+    <page-default :config="templateConfig">
+      <!-- ========================= title ========================= -->
       <template v-slot:title>
         <div class="text-3xl">Tự đánh giá</div>
       </template>
-      <!-- ======== title ======== -->
+      <!-- ========================= title ========================= -->
+      <!-- ========================= content-0 ========================= -->
+      <template v-slot:content-0>
+        <div class="px-12">
+          <div>
+            <el-divider content-position="left">
+              <div class="font-bold text-lg">Câu hỏi 1</div>
+            </el-divider>
+
+            <div class="text-base font-normal">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </div>
+
+            <div class="my-3"></div>
+
+            <el-input placeholder="Please input"></el-input>
+          </div>
+
+          <card-question> </card-question>
+        </div>
+      </template>
+      <!-- ========================= content-0 ========================= -->
     </page-default>
   </div>
 </template>
@@ -13,18 +43,16 @@
 <script>
 export default {
   data: () => ({
-    pageData: {
-      dialog: {},
-    },
+    templateConfig: null,
   }),
 
   mounted() {
-    this.initPageData();
+    this.initTemplateConfig();
   },
 
   methods: {
-    initPageData() {
-      this.pageData = {
+    initTemplateConfig() {
+      this.templateConfig = {
         dialog: {
           title: "Thông tin chi tiết",
           visible: false,
